2bb6ac6db37ae8d08cd08e6a5286a450aa3de832
Lawrence-Fasold <sylandia1@gmail.com>
2025-01-21 14:23:54 -0500

    RF Notes and Intial pass at packet strucutre
    Setting up and jotting down some higher level stuff to consider for RF. Not complete.
    
    Start working at packet strucutre. Will probs be changed some as we TI strucutre might override some.
    
    Also included some typedefs for unsigned int values in blink to be shared as common. Will need to update types for variables to match.

--- Keil_CC251x/Common_Shared/blink.h
+++ Keil_CC251x/Common_Shared/blink.h
@@ -3,6 +3,20 @@
 #ifndef BLINK_H
 #define BLINK_H
 
+// Define data types 
+/* 
+	If I am correct, Keil compiles int as 2 bytes. 
+	This means that uint16 = int type, uint32 = long int,
+	and uint64 = long long int. 
+*/ 
+typedef unsigned char uint8_t; 
+typedef unsigned int uint16_t; 
+typedef unsigned long int uint32_t; 
+typedef unsigned long long int uint64_t; 
+
+
+
+// Generalized blink and wait functions. 
 void wait(void);
 void blink(void);
 
--- Keil_CC251x/Handlers/cc_packet_handlers.h
+++ Keil_CC251x/Handlers/cc_packet_handlers.h
@@ -3,23 +3,40 @@
 #define PACKET_HANDLERS_H
 
 //defining unit8_t
-typedef unsigned char uint8_t; 
-typedef unsigned int uint16_t; 
+//typedef unsigned char uint8_t; 
+//typedef unsigned int uint16_t; 
 
 // defining start and end frame
 #define SOF 0x7E
 #define EOF 0x7F
 
 // Packet strucutre 
-typedef xdata struct{
+struct xdata kissPacketUart{
 
-	// include SOF? //1 byte
+	// include SOF? 
+	uint8_t sof; //1 byte
 	uint8_t command; // 1 byte
 	uint8_t lenght;  // 1 byte 
 	uint8_t payloadBuffer[50]; // 1 byte 
-	uint16_t crc;  // 2 bytes UART and 16 bytes RF? 
-	// include EOF? 
-}packet;
+	uint16_t crc;  // 2 bytes UART and 4 bytes RF? 
+	uint8_t eof; //1 byte 
+	//include EOF
+};
+
+struct xdata kissPacketRF {
+	
+	// include SOF? 
+	uint8_t sof; //1 byte
+	uint8_t command; // 1 byte
+	uint8_t lenght;  // 1 byte 
+	uint8_t payloadBuffer[50]; // 1 byte 
+	uint32_t crc;  // 4 bytes UART and 4 bytes RF? 
+	uint8_t eof; //1 byte 
+	//include EOF
+	
+	
+};
+
 
 
 void rfPacketHandler(void);
